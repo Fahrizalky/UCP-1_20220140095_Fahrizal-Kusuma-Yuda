891d93a0a7fc07c77fa3212f872dac42dc2d0591
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2024-05-06 22:54:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function showAlert() {
+    alert("Tidak perlu sempurna untuk menginspirasi orang lain. Biarkan orang lain terinspirasi oleh bagaimana kamu menangani ketidaksempurnaanmu-FahrizalKY")
+
+}
